1bdcc3bc79b0ceee616564e986f516fa60ed1708
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-17 23:54:26 -0500

    Refactor Users into a stateless functional component.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,14 +6,12 @@ import LoadingIcon from '../LoadingIcon/LoadingIcon';
 
 import './Users.css';
 
-class Users extends React.Component {
-  render() {
-    return (
-      <section className="users">
-        {this.props.users.length ? this.props.users.map(user => (<UserCard key={user.id} {...user} />)) : <LoadingIcon /> }
-      </section>
-    );
-  } 
+function Users(props) {
+  return (
+    <section className="users">
+      {props.users.length ? props.users.map(user => (<UserCard key={user.id} {...user} />)) : <LoadingIcon /> }
+    </section>
+  );
 }
 
 Users.propTypes = {
