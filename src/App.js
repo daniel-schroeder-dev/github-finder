1ed8ea1519cde3633bd2bbe40a88faaf0fdd73fa
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-18 11:43:40 -0500

    Use Basic Authentication when fetching data from GitHub api.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Users from './components/Users/Users';
 
 import './App.css';
 
+const githubCredentials = btoa(`${process.env.REACT_APP_GITHUB_CLIENT_ID}:${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
 class App extends React.Component {
 
   state = {
@@ -12,7 +14,12 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    fetch('https://api.github.com/users')
+    fetch('https://api.github.com/users', {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Basic ${githubCredentials}`,
+      },
+    })
       .then(res => res.json())
       .then(users => {
         this.setState({ users })
