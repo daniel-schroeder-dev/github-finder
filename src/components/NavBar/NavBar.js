d1cf9ed7bec4a4952f854a16c7f07c36beec9d21
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-17 23:05:36 -0500

    Refactor class components into functional components.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,16 +5,12 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import './NavBar.css';
 
-class NavBar extends React.Component {
-  
-  render() {
-    return (
-      <nav className="nav-bar">
-        <h1 className="nav-bar__site-title"><FontAwesomeIcon className="nav-bar__gh-icon" icon={faGithub} />GitHub Finder</h1>
-      </nav>
-    );
-  } 
-
+function NavBar(props) {
+  return (
+    <nav className="nav-bar">
+      <h1 className="nav-bar__site-title"><FontAwesomeIcon className="nav-bar__gh-icon" icon={faGithub} />GitHub Finder</h1>
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 
 import './UserCard.css';
 
-class UserCard extends React.Component {
-  render() {
-    return (
-      <article className="user-card">
-        <img className="user-card__avatar" src={this.props.avatar_url} alt="user avatar" />
-        <h2 className="user-card__login">{this.props.login}</h2>
-        <a className="user-card__html-url" href={this.props.html_url}>More</a>
-      </article>
-    );
-  } 
+function UserCard(props) {
+  return (
+    <article className="user-card">
+      <img className="user-card__avatar" src={props.avatar_url} alt="user avatar" />
+      <h2 className="user-card__login">{props.login}</h2>
+      <a className="user-card__html-url" href={props.html_url}>More</a>
+    </article>
+  );
 }
 
 UserCard.propTypes = {
