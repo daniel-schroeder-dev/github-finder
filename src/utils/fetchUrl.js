8dd07fa028eebc2c421468a02c5e7f60dbe72e9a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-18 13:38:26 -0500

    Use OAuth token instead of Basic Auth.

--- src/utils/fetchUrl.js
+++ src/utils/fetchUrl.js
@@ -1,10 +1,8 @@
-const githubCredentials = btoa(`${process.env.REACT_APP_GITHUB_CLIENT_ID}:${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
 const fetchUrl = url => {
   return fetch(url, {
     headers: {
       Accept: 'application/vnd.github.v3+json',
-      Authorization: `Basic ${githubCredentials}`,
+      Authorization: `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
     },
   });
 };
