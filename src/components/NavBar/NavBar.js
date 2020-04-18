ea111b95265984a2064f1c16bcc41564ea6ed529
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-17 22:08:07 -0500

    Add FontAwesome react icons and add GitHub icon to NavBar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import './NavBar.css';
 
 class NavBar extends React.Component {
@@ -6,7 +10,8 @@ class NavBar extends React.Component {
   render() {
     return (
       <nav className="nav-bar">
-        <h1 className="nav-bar__site-title">GitHub Finder</h1>
+
+        <h1 className="nav-bar__site-title"><FontAwesomeIcon className="nav-bar__gh-icon" icon={faGithub} />GitHub Finder</h1>
       </nav>
     );
   } 
