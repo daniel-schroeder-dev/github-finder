f6b28974e0d88060b75bed520f8fa42510971a0c
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-17 21:26:38 -0500

    Create basic NavBar component and add base styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import NavBar from './components/NavBar/NavBar';
+
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <NavBar />
       </div>
     );
   }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+class NavBar extends React.Component {
+  
+  render() {
+    return (
+      <nav className="nav-bar">
+        <h1 className="nav-bar__site-title">GitHub Finder</h1>
+      </nav>
+    );
+  } 
+
+}
+
+export default NavBar;
