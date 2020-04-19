e32d1906d686caa4a26157669dfd1aeb8fee7356
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-19 12:04:03 -0500

    Make site logo into a link to the HomePage.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -8,7 +9,9 @@ import './NavBar.css';
 function NavBar(props) {
   return (
     <nav className="nav-bar">
-      <h1 className="nav-bar__site-title"><FontAwesomeIcon className="nav-bar__gh-icon" icon={faGithub} />GitHub Finder</h1>
+      <Link className="nav-bar__home-page-link" to="/">
+        <h1 className="nav-bar__site-title"><FontAwesomeIcon className="nav-bar__gh-icon" icon={faGithub} />GitHub Finder</h1>
+      </Link>
     </nav>
   );
 }
